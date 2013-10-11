c47dcf0265d33cb6b2696b820a0276265f784272
Guest <guest-xTn2UT@meet.mit.edu>
2013-10-11 15:42:23 +0200

    finally

--- lap2/divisors.py
+++ lap2/divisors.py
@@ -1,6 +1,14 @@
-n=raw_input("give me number")
-for a in range(int(n)) :
-	#print a+1
-	if int(n) % (a+1) ==0 :
-		print a+1 
+#def divisors (n,num) :
+#	if n % (num+1)==0 :
+#		print num+1
+#
+#n=int(raw_input("give me number"))
+#divisors (n,10) 
 
+def divisor(n) :
+	for a in range(int(n))  : 
+		if n % (1+a) == 0 : 
+			print a+1
+
+y=int(raw_input("give me a number"))
+divisor(y)
