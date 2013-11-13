86ac0901c2c0165b5aaa6c4384c3fe5eda098ee3
Guest <guest-1hpFYw@meet.mit.edu>
2013-11-13 19:11:50 +0200

    wow

--- lap2/divisors.py
+++ lap2/divisors.py
@@ -2,5 +2,7 @@ def divisor(n) :
 	for a in range(int(n))  : 
 		if n % (1+a) == 0 : 
 			print a+1
-				y=int(raw_input("give me a number"))
-					divisor(1000)
+
+if __name__=="__main__"   :
+	y=int(raw_input("give me a number"))
+	divisor(y)
